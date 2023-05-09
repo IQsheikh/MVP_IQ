898be8e54df9ccbfabf9d697818de67927f5c372
Iqra Qadeer <iqra_afaq@hotmail.com>
2023-05-09 10:43:47 +0200

    first modification

---
+++ client/src/Cities.js
@@ -0,0 +1,53 @@
+const cities = [
+
+    {
+    id: 1,
+    city: "Antwerp",
+    province: "Antwerpen",
+    trainAccess: "Yes",
+    language: "Dutch",
+    population: 530504,
+    winter: "Christmas Market, Antwerpen Central Station, MAS",
+    summer: "Antwerpen Central Station, Antwerpen Zoo, Het Steen, De Meir",
+    spring: "Rebens Huis, Antwerpen Zoo, St Anna's Tunnel, Het Steen",
+    autumn: "Rebens Huis, Red Star Line Museum, Stadsfeestzaal"
+     },
+
+     {
+    city: "Brussels",
+    province: "Region of Brussels",
+    trainAccess: "Yes, 3 train stations",
+    language: "French & Dutch",
+    population: 1212352,
+    winter: "Christmas Market, Bright Brussels, Atomium",
+    summer: " Art & History Museum, Atomium, Royal Palace, Manekken Pis, Mini Europe",
+    spring: "Royal Greenhouses of Laeken, Hallerbos, Flower cartpet at Grand Place, , Mini Europe",
+    autumn: "Planetaruim, Cathedral of St. Michael & st. Gudula, Sonian Forest"
+     },
+
+     {
+    city: "Brugge",
+    province: "West Flanders",
+    trainAcces: "Yes",
+    language: "Dutch",
+    population: 118509,
+    winter: "Christmas Market, Shopping Street, Gruuthuse Museum, Brugge Light Show",
+    summer: " Boat Tours, Horese and Carriage Tours, Historic Center",
+    spring: "Tulips along canal, Boat tours, Belfry of Brugges",
+    autumn: "Autumn along the canal, Basilica of the Holy Blood, Sonian Forest, Bruge Train Station"
+     },
+
+     {
+    city: "Dinant",
+    province: "Namur",
+    trainAccess: "Yes",
+    language: "French",
+    population: 13544,
+    winter: "Christmas Market, Maison Leffe, Citadel of Dinant, Charles de Gaulle Bridge",
+    summer: " Boat Tours, Water Taxi, Charles De Gaulle Bridge, Castle Veves",
+    spring: "Citadel of Dinant, Dinant Adventure, Parc de Furfooz",
+    autumn: "Notre Dame de Dinant, Drive along river Meuse, Les Jardins d'Annevoie"
+        }
+]
+
+export default cities;
