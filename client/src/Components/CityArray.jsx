2bafdd14f05e00f1c54e567a97cbe82e2c619ff7
Iqra Qadeer <iqra_afaq@hotmail.com>
2023-05-22 10:26:15 +0200

    sixth commit

--- client/src/Components/CityArray.jsx
+++ client/src/Components/CityArray.jsx
@@ -1,44 +1,3 @@
-/*
-import React, { useState } from 'react';
-import cities from './Cities';
-import './CityArray.css';
-import CityDetails from './CityDetails';
-
-
-function cityArray(city) {
-  const [selectedCity, setSelectedCity] = useState(null);
-
-
-  const handleCityDetailsClick = (city) => {
-    setSelectedCity(city);
-  };
-
-
- 
-
-  return (
-    <div className="city-container">
-    
-    {cities.map((city) => (
-        <div className="city-card" key={city.id}>
-          <h2>{city.name}</h2>
-          <div className="city-details">
-            <p>Train Access: {city.trainAccess ? `Yes, ${city.trainAccess}` : 'No'}</p>
-          </div>
-    <img src={city.url} className="city-image" alt={city.name} /> 
-    <button onClick={() => handleCityDetailsClick(city)}>More Details</button> 
-        </div>
-      ))};
-
-      {selectedCity && <CityDetails city={selectedCity} />} 
-    
-    </div>
-  );
-  }
-
-export default cityArray; */
-
-
 import React, { useState } from 'react';
 import cities from './Cities';
 import './CityArray.css';
@@ -47,18 +6,12 @@ import { useNavigate } from 'react-router-dom';
 function cityArray(city) {
 
   const navigate = useNavigate ();
-  //const [selectedCity, setSelectedCity] = useState(null);
-  //const [reset, setReset] = useState(true);
 
 
   const handleCityDetailsClick = (city) => {
-   // setSelectedCity(city);
     navigate(`/city/${city.id}`);
   };
 
- // const handleResetCB = () => {
-    //setReset(false);
- // };
 
   return (
     <div className="city-container">
@@ -73,8 +26,6 @@ function cityArray(city) {
     <button onClick={() => handleCityDetailsClick(city)}>More Details</button> 
        </div>
       ))};
-
-    {/* {selectedCity && <CityDetails city={selectedCity} handleResetCB={() => handleResetCB(false)}/>} */}
       
 
 
